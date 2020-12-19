83fdd410b02d4e1b4ac0993904b702597ad2bceb
Doug Bridgens <thisdougb@users.noreply.github.com>
2020-12-19 13:39:32 +0000

    adds fetching secrets

--- pkg/datastore/hashicorpvault/fetchsecret.go
+++ pkg/datastore/hashicorpvault/fetchsecret.go
@@ -2,11 +2,33 @@ package hashicorpvault
 
 import (
 	"context"
+	"fmt"
+	"github.com/hashicorp/vault/api"
 	"log"
 )
 
 // FetchSecret fetches the secret(s) for an id
 func (d *Datastore) FetchSecret(ctx context.Context, id string) (string, error) {
-	log.Println("hashicorpvault.FetchSecret(): store for id ", id)
+
+	log.Println("hashicorpvault.FetchSecret(): fetch secrets for id ", id)
+
+	path := fmt.Sprintf(defaultIdentitySecretKeyFormat, id)
+
+	var secret *api.Secret
+
+	l := d.client.Logical()
+	secret, err := l.Read(path)
+	if err != nil {
+		return "", err
+	}
+
+	if data, ok := secret.Data["data"]; ok {
+
+		m := data.(map[string]interface{})
+		value := m["secret"].(string)
+
+		return value, nil
+	}
+
 	return "", nil
 }
